4863c51004e312b1d26db5da472047eedb6bf6d4
skepticaldev <philipe.cavalcanti.lp@gmail.com>
2019-08-18 08:24:35 -0300

    chore: prevent duplicate user insert db

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -5,6 +5,14 @@ module.exports = {
   async store(req, res) {
     const { username } = req.body;
 
+    const userExists = await Dev.findOne({
+      user: username
+    });
+
+    if (userExists) {
+      return res.json(userExists);
+    }
+
     const response = await axios.get(
       `https://api.github.com/users/${username}`
     );
